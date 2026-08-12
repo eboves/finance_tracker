d3eb63e4559a5e80e0f7c2d23367db1b183828f3
Elvis <e.boves@gmail.com>
2026-08-12 19:56:21 -0400

    feat: working account insert

--- add_account.py
+++ add_account.py
@@ -1,1 +1,11 @@
+from database import get_connection
 
+conn = get_connection()
+cur = conn.cursor()
+
+cur.execute("INSERT INTO accounts (name, account_type, institution, date_opened) VALUES (%s, %s, %s, %s)", ("retirement roth ira", "roth_ira", "fidelity", "2012-03-08"))
+
+conn.commit()
+
+cur.close()
+conn.close()
--- db_test.py
+++ db_test.py
@@ -1,14 +1,12 @@
-from dotenv import load_dotenv
-import os
-import psycopg2
 from database import get_connection
 
-db_test = get_connection()
+conn = get_connection()
 
-cur = db_test.cursor()
-cur.execute("INSERT INTO accounts (name, account_type, institution, date_opened) VALUES (%s, %s, %s, %s)", ("amex_savings", "savings", "american express", "2010-08-07"))
+cur = conn.cursor()
+cur.execute("SELECT version();")
+version = cur.fetchone()[0]
 
-db_test.commit()
+print(version)
 
 cur.close()
-db_test.close()
+conn.close()
