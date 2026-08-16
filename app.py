dc06fc97cb1c81d73760f9805d288dcae0fdc900
Elvis <e.boves@gmail.com>
2026-08-16 06:55:45 -0400

    feat: add /accounts and /balances route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from database import get_accounts
+from database import get_accounts, get_balances
 
 app = Flask(__name__)
 
@@ -12,6 +12,10 @@ def get_accounts_route():
     accounts = get_accounts()
     return jsonify(accounts)
 
+@app.route("/balances")
+def get_balances_route():
+    balances = get_balances()
+    return jsonify(balances)
 
 if __name__ == "__main__":
     app.run(debug=True)
